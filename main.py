770de77a62216e37caf3c2dfbafd8ffc94c3252f
Joy James <joyjames@techie.com>
2019-05-23 18:34:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+mood = input("How are you?")
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Could be worse...")
+elif mood == "excited":
+    print("Go for it!")
+elif mood == "relaxed":
+    print("Feeling good?")
+else:
+    print("I don't recognize this mood")
